1a4f39004dd06cc17e875ca740462af323b7574b
chaimakhalfaoui <chaimakhalfellaoui01@gmail.com>
2026-04-27 12:36:16 +0200

    Ajout du pipeline CI/CD complet

--- Back/routes/qr.js
+++ Back/routes/qr.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createMessage,getMessages } from "../controllers/QR.js";
+import { createMessage, getMessages } from "../controllers/qr.js";
 
 
 const router = express.Router();
