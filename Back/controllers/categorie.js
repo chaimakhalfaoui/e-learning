e69bd7554c1c1ba7a7cc5de80f6e5bc0a874865b
chaimakhalfaoui <chaimakhalfellaoui01@gmail.com>
2026-05-01 12:47:34 +0200

    fix: use dedicated uploads bucket for S3 storage

--- Back/controllers/categorie.js
+++ Back/controllers/categorie.js
@@ -5,7 +5,7 @@ import { S3Client } from "@aws-sdk/client-s3";
 import { v4 as uuidv4 } from "uuid";
 
 const s3 = new S3Client({ region: "us-east-1" });
-const BUCKET = "isetso-frontend-378174569462";
+const BUCKET = "isetso-uploads-378174569462";
 
 const storage = multerS3({
   s3,
