0da3ae8bc4f50f54220d20d88a7bb866a1f441d9
chaimakhalfaoui <chaimakhalfellaoui01@gmail.com>
2026-05-20 01:36:18 +0200

    Fix images: correction finale avec startsWith

--- front/src/utils/imageUtils.js
+++
@@ -1,5 +0,0 @@
-export const getImageUrl = (imagePath) => {
-  if (!imagePath) return "/placeholder.svg";
-  if (imagePath.startsWith("http")) return imagePath;
-  return `http://isetso-backend-lb-617645434.us-east-1.elb.amazonaws.com/api/image/${imagePath}`;
-};
